0b1c23a33e0bc0d07d9e804e7cd885a64f3833fe
Javi <suavejavi@gmail.com>
2020-01-11 00:33:25 -0500

    feat: add project block component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import { MatDividerModule } from "@angular/material/divider";
 
 @NgModule({
-    imports: [MatCardModule],
-    exports: [MatCardModule],
+    imports: [MatCardModule, MatDividerModule],
+    exports: [MatCardModule, MatDividerModule],
 })
 export class MaterialModule { }
