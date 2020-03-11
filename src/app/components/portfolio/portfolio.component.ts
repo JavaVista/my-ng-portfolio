761632f1dc5a11b5dcbcbfee1ad477ee120d25ce
Javi <suavejavi@gmail.com>
2020-03-10 22:51:56 -0400

    feat: add portfolio animations

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/services/portfolio.service';
 import { Portfolio } from 'src/app/services/portfolio.model';
+import { trigger, transition, animate, style, query, stagger, animateChild } from '@angular/animations';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.sass']
+  styleUrls: ['./portfolio.component.sass'],
+  animations: [
+        trigger('list',[
+            transition(':enter', [
+               query('@items', stagger(60, animateChild()))
+        ])
+      ]),
+        trigger('items', [
+            transition(':enter', [
+            style({transform:'scale(0)', opacity: 0}),
+            animate('0.350s cubic-bezier(.8, -0.6, 0.2, 1.5)',
+            style({transform: 'scale(1)', opacity: 1}))
+        ])
+      ])
+  ]
 })
 export class PortfolioComponent implements OnInit {
 
